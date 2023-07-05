417f90e853c2527224fcc009e0c96d601533fa3e
Aleksandar Dimov <isavitaisa@gmail.com>
2023-07-05 20:12:43 +0100

    Working app without supervisor

--- src/event.erl
+++ src/event.erl
@@ -1,28 +1,18 @@
 -module(event).
--compile(export_all).
--record(state, {server, name = "", to_go = 0}).
+-export([start/2, start_link/2, cancel/1]).
+-export([init/3, loop/1]).
+-record(state, {
+    server,
+    name = "",
+    to_go = 0
+}).
 
+%%% Public interface
 start(EventName, DateTime) ->
     spawn(?MODULE, init, [self(), EventName, DateTime]).
 
 start_link(EventName, DateTime) ->
     spawn_link(?MODULE, init, [self(), EventName, DateTime]).
-
-init(Server, EventName, DateTime) ->
-    loop(#state{server = Server, name = EventName, to_go = time_to_go(DateTime)}).
-
-loop(S = #state{server = Server, to_go = [T | Next]}) ->
-    receive
-        {Server, Ref, cancel} ->
-            Server ! {Ref, ok}
-    after T * 1000 ->
-        if
-            Next =:= [] ->
-                Server ! {done, S#state.name};
-            Next =/= [] ->
-                loop(S#state{to_go = Next})
-        end
-    end.
 
 cancel(Pid) ->
     %% Monitor in case the process is already dead
@@ -36,6 +26,30 @@ cancel(Pid) ->
             ok
     end.
 
+%%% Event's innards
+init(Server, EventName, DateTime) ->
+    loop(#state{
+        server = Server,
+        name = EventName,
+        to_go = time_to_go(DateTime)
+    }).
+
+%% Loop uses a list for times in order to go around the ~49 days limit
+%% on timeouts.
+loop(S = #state{server = Server, to_go = [T | Next]}) ->
+    receive
+        {Server, Ref, cancel} ->
+            Server ! {Ref, ok}
+    after T * 1000 ->
+        if
+            Next =:= [] ->
+                Server ! {done, S#state.name};
+            Next =/= [] ->
+                loop(S#state{to_go = Next})
+        end
+    end.
+
+%%% private functions
 time_to_go(TimeOut = {{_, _, _}, {_, _, _}}) ->
     Now = calendar:local_time(),
     ToGo =
@@ -44,10 +58,12 @@ time_to_go(TimeOut = {{_, _, _}, {_, _, _}}) ->
     Secs =
         if
             ToGo > 0 -> ToGo;
-            true -> 0
+            ToGo =< 0 -> 0
         end,
     normalize(Secs).
 
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used
 normalize(N) ->
     Limit = 49 * 24 * 60 * 60,
     [N rem Limit | lists:duplicate(N div Limit, Limit)].
